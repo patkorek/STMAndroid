b3b4423b7ae8b66981bb0c88d86db2129419978c
Patrycjusz Korek <patkorek@gmail.com>
2018-10-30 19:19:25 +0100

    added soap constans

--- app/src/main/java/eti/pg/stm/MainActivity.java
+++ app/src/main/java/eti/pg/stm/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String NAMESPACE = "http://example/";
+    private static String URL = "http://localhost:8081/services/HelloWorld?wsdl";
+    private static final String METHOD_NAME = "sayHelloWorldFrom";
+    private static final String SOAP_ACTION =  "http://example/sayHelloWorldFrom";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
